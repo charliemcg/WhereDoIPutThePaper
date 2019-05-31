4d30a7b590519de03c23a4c211d6e031dba718fd
macbookair <macbookair@GLM1076892.localdomain>
2019-05-31 23:49:06 +0800

    committing minor changes before merging to master

--- src/components/Home/styles/phone.js
+++ src/components/Home/styles/phone.js
@@ -30,8 +30,8 @@ export default {
     flex: 2
   },
   flagDimensions: {
-    width: height * 0.1,
-    height: height * 0.1
+    width: height * 0.09,
+    height: height * 0.09
   },
   picker: {
     height: "50%",
